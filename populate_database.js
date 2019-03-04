d0266a72cca2aa957e0c56ea699235860bef63cc
CAILLON Romain <romain.caillon@devinci.fr>
2019-03-04 18:22:31 +0100

    add populate_database.js

---
+++ populate_database.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console, no-process-exit */
+const imdb = require('./src/imdb');
+const DENZEL_IMDB_ID = 'nm0000243';
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://romain:QXTmUHic3AJkONQK@cluster0-4c157.mongodb.net/test?retryWrites=true"
+
+async function sandbox(actor) {
+  try {
+    const movies = await imdb(actor);
+    const client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+      const collection = client.db("movies").collection("movies");
+      collection.insert(movies);
+      client.close();
+    });
+
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+sandbox(DENZEL_IMDB_ID);
