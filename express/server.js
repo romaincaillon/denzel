d467e54300b7eb0f4a2be51b1b69559f10d54dd8
CAILLON Romain <romain.caillon@devinci.fr>
2019-03-20 12:10:50 +0100

    netlify

---
+++ express/server.js
@@ -0,0 +1,102 @@
+const Express = require("express");
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+
+const GraphqlHTTP = require('express-graphql');
+const {GraphQLSchema} = require('graphql');
+const {queryType} = require('../query.js');
+
+const imdb = require('../src/imdb');
+const DENZEL_IMDB_ID = 'nm0000243';
+
+const CONNECTION_URL = "mongodb+srv://romain:QXTmUHic3AJkONQK@cluster0-4c157.mongodb.net/test?retryWrites=true";
+const DATABASE_NAME = "movies";
+
+var app = Express();
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+var database, collection;
+
+const schema = new GraphQLSchema({ query: queryType });
+app.use('/graphql', GraphqlHTTP({
+    schema: schema,
+    graphiql: true,
+}));
+
+app.listen(9292, () => {
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        collection = database.collection("movies");
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
+});
+
+app.get("/movies/populate", async (request, response) => {
+    try {
+      const movies = await imdb(DENZEL_IMDB_ID);
+      collection.insertMany(movies);
+      result = {
+        "total": movies.length
+      };
+      response.send(result);
+    } catch (e) {
+      console.error(e);
+      return response.status(500).send("Error trying to populate the database");
+    }
+});
+
+app.get("/movies", (request, response) => {
+    collection.find({"metascore": {$gte: 70}}).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result[Math.floor(Math.random() * result.length)]);
+    });
+});
+
+app.get("/movies/search", (request, response) => {
+    var limit = (request.query.limit === undefined ? 5 : parseInt(request.query.limit));
+    var metascore = (request.query.metascore === undefined ? 0 : parseInt(request.query.metascore));
+
+    collection.find({"metascore": {$gte: metascore}}).limit(limit).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
+
+app.get("/movies/:id", (request, response) => {
+    collection.findOne({ "id": request.params.id }, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
+
+app.post("/movies/:id", (request, response) => {
+    if(request.body.review === undefined || request.body.date === undefined) {
+        return response.status(400).send("You have to specify review and date");
+    }
+    collection.update({"id": request.params.id}, {$set: {"date": request.body.date, "review": request.body.review}}, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+    });
+    collection.findOne({"id": request.params.id}, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        result = {
+          "_id": result._id
+        };
+        response.send(result);
+    });
+});
